4118f638248ae1485d669a6db774094b55daf383
Alex Efros <powerman@powerman.name>
2019-04-15 01:25:48 +0300

    feat: add NewContext/FromContext

---
+++ context.go
@@ -0,0 +1,25 @@
+package structlog
+
+import "context"
+
+type contextKey int
+
+const contextKeyLog contextKey = 0
+
+// NewContext returns a new Context that carries value log.
+func NewContext(ctx context.Context, log *Logger) context.Context {
+	return context.WithValue(ctx, contextKeyLog, log)
+}
+
+// FromContext returns the Logger value stored in ctx or defaultLog or
+// New() if defaultLog is nil.
+func FromContext(ctx context.Context, defaultLog *Logger) *Logger {
+	switch log, _ := ctx.Value(contextKeyLog).(*Logger); true {
+	case log != nil:
+		return log
+	case defaultLog != nil:
+		return defaultLog
+	default:
+		return New()
+	}
+}
---
+++ context_test.go
@@ -0,0 +1,26 @@
+package structlog_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/powerman/check"
+	"github.com/powerman/structlog"
+)
+
+func TestContext(tt *testing.T) {
+	t := check.T(tt)
+	log1 := structlog.New()
+	log2 := structlog.New()
+	log3 := structlog.New()
+	ctx := context.Background()
+	t.NotNil(structlog.FromContext(ctx, nil))
+	t.HasType(structlog.FromContext(ctx, nil), log1)
+	t.Equal(structlog.FromContext(ctx, log1), log1)
+	ctx = structlog.NewContext(ctx, log2)
+	t.Equal(structlog.FromContext(ctx, nil), log2)
+	t.Equal(structlog.FromContext(ctx, log1), log2)
+	ctx = structlog.NewContext(ctx, log3)
+	t.Equal(structlog.FromContext(ctx, nil), log3)
+	t.Equal(structlog.FromContext(ctx, log1), log3)
+}
